162eeb5c6972c16acfff0a1a7080bd4be90486b7
Manar Safi <safi.manar@berkeley.edu>
2017-09-30 21:30:43 -0700

    Method call typo

--- attend.py
+++ attend.py
@@ -3,7 +3,7 @@ import CONSTANTS
 
 
 # Process the data message
-def process_message(messaging_event):
+def process_message_event(messaging_event):
     sender_id = messaging_event["sender"]["id"]  # the facebook ID of the person sending you the message
     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
     if messaging_event["message"].get("text"):
