e3081b570cd29ab50b095857c1bf0494c95cf569
Manar Safi <safi.manar@berkeley.edu>
2017-09-30 21:27:16 -0700

    Handling attachments so AttendyBot doesn't crash.

--- app.py
+++ app.py
@@ -36,13 +36,7 @@ def webhook():
             for messaging_event in entry["messaging"]:
 
                 if messaging_event.get("message"):  # someone sent us a message
-
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
-
-                    # Handle the message.
-                    attend.handle_message(sender_id, message_text)
+                    attend.process_message_event(messaging_event)
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
--- attend.py
+++ attend.py
@@ -1,6 +1,23 @@
 import send_wrapper as send
 import CONSTANTS
 
+
+# Process the data message
+def process_message(messaging_event):
+    sender_id = messaging_event["sender"]["id"]  # the facebook ID of the person sending you the message
+    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+    if messaging_event["message"].get("text"):
+        message_text = messaging_event["message"]["text"]  # the message's text
+        # Handle the message.
+        handle_message(sender_id, message_text)
+    elif messaging_event["message"].get("attachments"):
+        attachment = messaging_event["message"]["attachments"]
+        handle_attachment(sender_id, attachment)
+
+def handle_attachment(sender_id, attachment):
+    send.send_message(sender_id, "You have attempted to share an attachment.")
+    return
+
 def handle_message(sender_id, message):
     if message == CONSTANTS.HELP:
         handle_help(sender_id, message)
