e4a3a374310b39fae06cd0974302338b57c97af7
Manar Safi <safi.manar@berkeley.edu>
2017-09-30 20:52:36 -0700

    Added constants and more message handling for help and report

---
+++ CONSTANTS.py
@@ -0,0 +1,20 @@
+
+# A list of constants string constants
+
+# General Strings
+
+# List of available Commands
+ATTENDANCE = "attendance"
+REPORT = "report"
+HELP = "help"
+
+
+# Messages that Attendy Bot can send to the user
+
+ATTENDANCE_CONFIRMED = "Your attendance has been marked! (fake message)"
+
+HELP_MESSAGE = """Confused? Here are a list of commands you can use. \n
+{0} - Mark your attendance \n
+{1} - Show a report of your attendance for the semester
+{2} - Show the help menu
+""".format(ATTENDANCE, REPORT, HELP)
--- attend.py
+++ attend.py
@@ -1,10 +1,22 @@
 import send_wrapper as send
+import CONSTANTS
 
 def handle_message(sender_id, message):
-    if message == "Attendance":
-        message_temp = "Your attendance has been marked! (fake message)"
+    if message == CONSTANTS.HELP:
+        handle_help(sender_id, message)
+    elif message == CONSTANTS.ATTENDANCE:
+        message_temp = CONSTANTS.ATTENDANCE_CONFIRMED
         send.send_message(sender_id, message_temp)
+    elif message == CONSTANTS.REPORT:
+        handle_report(sender_id, message)
     else:
         send.send_message(sender_id, "Unknown command! Here is an echo of your message:")
         send.send_message(sender_id, message)
 
+def handle_help(sender_id, message):
+    send.send_message(sender_id, CONSTANTS.HELP_MESSAGE)
+    return
+
+def handle_report(sender_id, message):
+    send.send_message(sender_id, "PLACEHOLDER MESSAGE") #TODO
+    return
